980bf6334b9124bf7eb2ebcd19490ff85012094e
Josh Freeman <josh.freeman@sykescottages.co.uk>
2019-01-25 15:17:13 +0000

    Update key errror

--- src/Exception/ApiRestrictedException.php
+++ src/Exception/ApiRestrictedException.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 class ApiRestrictedException extends HttpException
 {
     const CODE    = 400;
-    const MESSAGE = 'Your API key does not have access to this endpoint.';
+    const MESSAGE = 'Could not find a valid key in the URL request. Please check the URL you are requesting against has the key field. It should look like this: ?something=ewf&key=your_key.';
 
     public function __construct()
     {
