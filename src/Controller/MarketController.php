a1a93c7808c536a29773d1f703e97fa528a8ded3
Josh Freeman <josh@viion.co.uk>
2019-05-03 17:55:01 +0100

    Update report logic

--- src/Controller/MarketController.php
+++ src/Controller/MarketController.php
@@ -70,11 +70,10 @@ class MarketController extends AbstractController
     {
         $criticalCount = $this->companionErrorHandler->getCriticalExceptionCount();
 
-        return $this->json([
-            'Stats'         => $this->companionStatistics->getStatistics(),
-            'IsCritical'    => $criticalCount > CompanionConfiguration::ERROR_COUNT_THRESHOLD,
-            'CriticalCount' => $criticalCount,
-        ]);
+        $report = $this->companionStatistics->getStatistics();
+        $report->IsCritical = $criticalCount > CompanionConfiguration::ERROR_COUNT_THRESHOLD;
+
+        return $this->json($report);
     }
     
     /**
