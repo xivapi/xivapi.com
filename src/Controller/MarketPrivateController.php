11295bba0bac2a56c07ca782837376165141b31d
Josh Freeman <josh@viion.co.uk>
2019-05-06 22:02:31 +0100

    Switch all checks to DC instead of server

--- src/Controller/MarketPrivateController.php
+++ src/Controller/MarketPrivateController.php
@@ -26,7 +26,7 @@ class MarketPrivateController extends AbstractController
     private $companionTokenManager;
     /** @var MarketUpdater */
     private $companionMarketUpdater;
-
+    
     public function __construct(
         CompanionTokenManager $companionTokenManager,
         MarketUpdater $companionMarketUpdater
@@ -34,7 +34,7 @@ class MarketPrivateController extends AbstractController
         $this->companionTokenManager  = $companionTokenManager;
         $this->companionMarketUpdater = $companionMarketUpdater;
     }
-
+    
     /**
      * @Route("/private/market/item")
      */
@@ -43,7 +43,7 @@ class MarketPrivateController extends AbstractController
         if ($request->get('access') !== getenv('MB_ACCESS')) {
             throw new UnauthorizedHttpException('Denied');
         }
-
+        
         $itemId = (int)$request->get('item_id');
         $server = (int)GameServers::getServerId(ucwords($request->get('server')));
         $key    = "companion_private_query_prices_{$itemId}_{$server}";
@@ -51,7 +51,7 @@ class MarketPrivateController extends AbstractController
         if ($response = Redis::Cache()->get($key)) {
             return $this->json($response);
         }
-
+        
         $token  = $this->companionTokenManager->getCompanionTokenForServer($server);
         $api    = new CompanionApi();
         $api->Token()->set((Object)$token->getToken());
@@ -60,7 +60,7 @@ class MarketPrivateController extends AbstractController
         
         return $this->json($response);
     }
-
+    
     /**
      * @Route("/private/market/item/history")
      */
@@ -69,11 +69,11 @@ class MarketPrivateController extends AbstractController
         if ($request->get('access') !== getenv('MB_ACCESS')) {
             throw new UnauthorizedHttpException('Denied');
         }
-
+        
         $itemId = (int)$request->get('item_id');
         $server = (int)GameServers::getServerId(ucwords($request->get('server')));
         $key    = "companion_private_query_history_{$itemId}_{$server}";
-    
+        
         if ($response = Redis::Cache()->get($key)) {
             return $this->json($response);
         }
@@ -83,10 +83,10 @@ class MarketPrivateController extends AbstractController
         $api->Token()->set((Object)$token->getToken());
         $response = $api->Market()->getTransactionHistory($itemId);
         Redis::Cache()->set($key, $response, 60);
-    
+        
         return $this->json($response);
     }
-
+    
     /**
      * This is used by alerts for DPS users.
      * @Route("/private/market/item/update")
@@ -96,29 +96,30 @@ class MarketPrivateController extends AbstractController
         if ($request->get('access') !== getenv('MB_ACCESS')) {
             throw new UnauthorizedHttpException('Denied');
         }
-
+        
         $itemId = (int)$request->get('item_id');
         $server = (int)$request->get('server');
-
+        $dc     = GameServers::getDataCenter(GameServers::LIST[$server]);
+        
         /** @var CompanionItem $marketEntry */
         $marketEntry = $this->companionMarketUpdater->getMarketItemEntry($server, $itemId);
-
+        
         /**
          * First, check if the item was passed here already
          */
-        $requestLastSent = Redis::Cache()->get("companion_market_manual_queue_check_{$itemId}_{$server}");
+        $requestLastSent = Redis::Cache()->get("companion_market_manual_queue_check_{$itemId}_{$dc}");
         if ($requestLastSent) {
             return $this->json([ false, $requestLastSent, 'This item has already been requested very recently to be updated, it should update shortly.' ]);
         }
-
+        
         // Place the item on this server in a cooldown
-        Redis::Cache()->set("companion_market_manual_queue_check_{$itemId}_{$server}", time(), (60 * 10));
+        Redis::Cache()->set("companion_market_manual_queue_check_{$itemId}_{$dc}", time(), (60 * 10));
         
         // if the item is already in the patreon queue, skip it
         if ($marketEntry->getPatreonQueue() > 0) {
             return $this->json([ false, $requestLastSent, 'This item is in a patron queue and will be updated shortly.' ]);
         }
-
+        
         /**
          * Check when the item was last updated, maybe it updated within the last 5 minutes,
          * if so then we don't need to update it again.
@@ -126,7 +127,7 @@ class MarketPrivateController extends AbstractController
         if ($marketEntry->getUpdated() > (time() - (60 * 10))) {
             return $this->json([ false, $marketEntry->getUpdated(), 'Item was updated recently (within 10 minutes) and cannot be updated at this time.' ]);
         }
-
+        
         /**
          * Pick a random queue, it should distribute mostly... evenly.
          */
@@ -138,14 +139,14 @@ class MarketPrivateController extends AbstractController
         
         foreach ($queues as $i => $num) {
             $size = Redis::Cache()->get("companion_market_manual_queue_{$num}");
-
+            
             if ($size === null) {
                 Redis::Cache()->set("companion_market_manual_queue_{$num}", 1, 60);
                 $queue = $num;
                 break;
             }
         }
-
+        
         /**
          * if we have a queue, use it, otherwise pick oen at random
          */
@@ -166,38 +167,42 @@ class MarketPrivateController extends AbstractController
         if ($request->get('access') !== getenv('MB_ACCESS')) {
             throw new UnauthorizedHttpException('Denied');
         }
-    
+        
         $itemId = (int)$request->get('item_id');
         $server = (int)$request->get('server');
+        $dc     = GameServers::getDataCenter(GameServers::LIST[$server]);
+        
+        /** @var CompanionItem $marketEntry */
+        $marketEntry = $this->companionMarketUpdater->getMarketItemEntry($server, $itemId);
         
         if (in_array($server, GameServers::MARKET_OFFLINE)) {
             return $this->json([ false, 0, 'The server provided is currently not supported.' ]);
         }
-    
-        /** @var CompanionItem $marketEntry */
-        $marketEntry = $this->companionMarketUpdater->getMarketItemEntry($server, $itemId);
         
         // if the item was updated within the hour, ignore
-        if ($marketEntry->getUpdated() > time() - (60 * 60)) {
+        if (Redis::Cache()->get("companion_item_dc_update_60minute_{$itemId}_{$dc}")) {
             return $this->json([ false, $marketEntry->getUpdated(), 'This item has already been updated within the past 60 minutes and cannot be bumped up the queue at this time.' ]);
         }
-    
+        
         /**
          * First, check if the item was passed here already
          */
-        $requestLastSent = Redis::Cache()->get("companion_market_manual_queue_check_{$itemId}_{$server}");
+        $requestLastSent = Redis::Cache()->get("companion_market_manual_queue_check_{$itemId}_{$dc}");
         if ($requestLastSent) {
             return $this->json([ false, $requestLastSent, 'This item has already been requested very recently to be updated, it should update shortly.' ]);
         }
-    
-        // Place the item on this server in a cooldown
-        Redis::Cache()->set("companion_market_manual_queue_check_{$itemId}_{$server}", time(), (60 * 10));
-    
+        
         // if the item is already in the patreon queue, skip it
         if ($marketEntry->getPatreonQueue() > 0) {
             return $this->json([ false, $requestLastSent, 'This item is in a patron queue and will be updated shortly.' ]);
         }
-    
+        
+        // Place the item on this server in a cooldown (for alerts)
+        Redis::Cache()->set("companion_market_manual_queue_check_{$itemId}_{$dc}", time(), (60 * 10));
+        
+        // Place the item on a 1 hour cooldown.
+        Redis::Cache()->set("companion_item_dc_update_60minute_{$itemId}_{$dc}", time(), (60 * 60));
+        
         // get servers for this DC
         $servers = GameServers::getDataCenterServers(GameServers::LIST[$server]);
         
@@ -208,7 +213,7 @@ class MarketPrivateController extends AbstractController
             $marketEntry->setPriority(0);
             $this->companionMarketUpdater->saveMarketItemEntry($marketEntry);
         }
-    
+        
         return $this->json([
             true,
             time(),
