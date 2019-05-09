41a92e089245f1401a0832888fee4ef26814c2e4
Josh Freeman <josh@viion.co.uk>
2019-05-09 18:14:45 +0100

    Merge fc icons

--- src/Controller/LodestoneFreeCompanyController.php
+++ src/Controller/LodestoneFreeCompanyController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Exception\ContentGoneException;
+use App\Service\Api\Response;
 use App\Service\Lodestone\FreeCompanyService;
 use App\Service\Lodestone\ServiceQueues;
 use App\Service\LodestoneQueue\FreeCompanyQueue;
+use Intervention\Image\ImageManager;
 use Lodestone\Api;
 use App\Service\Redis\Redis;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -100,5 +102,54 @@ class LodestoneFreeCompanyController extends AbstractController
         
         Redis::Cache()->set(__METHOD__.$lodestoneId, 1, ServiceQueues::UPDATE_TIMEOUT);
         return $this->json(1);
+    }
+
+    /**
+     * @Route("/FreeCompany/{lodestoneId}/Icon")
+     * @Route("/freecompany/{lodestoneId}/icon")
+     */
+    public function icon($lodestoneId)
+    {
+        $lodestoneId = strtolower(trim($lodestoneId));
+
+        $freecompany = $this->service->get($lodestoneId);
+
+        if ($freecompany == null) {
+            throw new \Exception('FC not found');
+        }
+
+        /**
+         * Filename for FC icon
+         */
+        $filename = __DIR__.'/../../public/fc/'. $freecompany->ID .'.png';
+
+        /**
+         * Check if it exists, if so, spit it out
+         */
+        if (file_exists($filename)) {
+            return new Response($filename, 200);
+        }
+
+        /** @var ImageManager $manager */
+        $manager = new ImageManager(['driver' => 'imagick']);
+        $img = $manager->make($filename);
+
+        /**
+         * Merge the 3 crests
+         */
+        $img->insert(
+            $manager->make($freecompany->Crest[0]),
+            $manager->make($freecompany->Crest[1]),
+            $manager->make($freecompany->Crest[2])
+        );
+
+        /**
+         * Save and compress
+         */
+        $img->save($filename);
+        $img = imagecreatefrompng($filename);
+        imagejpeg($img, $filename, 95);
+
+        return new Response($filename, 200);
     }
 }
