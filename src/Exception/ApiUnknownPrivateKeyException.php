094f186b9a33743a5296d659e5755ffc1bd52739
Josh Freeman <josh@viion.co.uk>
2019-05-12 20:38:19 +0100

    API exception

--- src/Exception/ApiUnknownPrivateKeyException.php
+++ src/Exception/ApiUnknownPrivateKeyException.php
@@ -1,13 +1,14 @@
 <?php
 
-namespace App\Exception;
+namespace App\Exceptions;
 
+use App\Common\Exceptions\ExceptionTrait;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class ApiUnknownPrivateKeyException extends HttpException
 {
     use ExceptionTrait;
-    
+
     const CODE    = 401;
     const MESSAGE = 'Could not find a user for this key, please check your key or remove it.';
 }
