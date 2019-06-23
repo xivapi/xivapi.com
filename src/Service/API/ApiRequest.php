48d204b42c29d5c73386814104150d0eb295f0ba
Josh Freeman <josh@viion.co.uk>
2019-06-23 19:00:21 +0100

    Increase public rate limit

--- src/Service/API/ApiRequest.php
+++ src/Service/API/ApiRequest.php
@@ -31,7 +31,7 @@ class ApiRequest
 {
     const KEY_FIELD             = 'private_key';
     const MAX_RATE_LIMIT_KEY    = 30;
-    const MAX_RATE_LIMIT_GLOBAL = 5;
+    const MAX_RATE_LIMIT_GLOBAL = 12;
     
     /**
      * List of controllers that require a API Key
