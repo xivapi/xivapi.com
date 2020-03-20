aede6db3dd14d8e9553f3b877ce6e41182d81c5f
Supamiu <contact@flavien-normand.fr>
2020-03-20 11:06:24 +0100

    Add GatheringPoint custom data mapper

---
+++ src/Service/DataCustom/GatheringPoint.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Service\DataCustom;
+
+use App\Service\Content\ManualHelper;
+use App\Common\Service\Redis\Redis;
+
+class Orchestrion extends ManualHelper
+{
+    const PRIORITY = 21;
+    
+    public function handle()
+    {
+        $ids = $this->getContentIds('GatheringPoint');
+    
+        foreach ($ids as $id) {
+            $key = "xiv_GatheringPoint_{$id}";
+            
+            // append GatheringPointTransient
+            $GatheringPoint = Redis::Cache()->get($key);
+            $GatheringPoint->GatheringPointTransient = Redis::Cache()->get("xiv_GatheringPointTransient_{$id}");
+            
+            // save
+            Redis::Cache()->set($key, $GatheringPoint, self::REDIS_DURATION);
+        }
+    }
+}
