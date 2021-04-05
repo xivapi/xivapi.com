fbbc33fefdcdfe9f291f37a936a712a0af260450
Supamiu <contact@flavien-normand.fr>
2021-04-05 11:56:58 +0200

    chore: increase in rate limits

--- src/Common/Constants/RateLimitConstants.php
+++ src/Common/Constants/RateLimitConstants.php
@@ -5,6 +5,6 @@ namespace App\Common\Constants;
 class RateLimitConstants
 {
     const KEY_FIELD             = 'private_key';
-    const MAX_RATE_LIMIT_KEY    = 20;
-    const MAX_RATE_LIMIT_GLOBAL = 8;
+    const MAX_RATE_LIMIT_KEY    = 30;
+    const MAX_RATE_LIMIT_GLOBAL = 20;
 }
