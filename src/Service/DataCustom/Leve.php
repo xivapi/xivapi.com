a49da34f9a481ce33fccf2e727cc8cdaec26adfc
Flavien Normand <contact@flavien-normand.fr>
2022-04-29 11:42:39 +0200

    fix'Leve): fixed custom importer

--- src/Service/DataCustom/Leve.php
+++ src/Service/DataCustom/Leve.php
@@ -18,34 +18,33 @@ class Leve extends ManualHelper
             // ---------------------------------------------------
 
             // defaults
-            // $leve->CraftLeve        = null;
+            $leve->CraftLeve        = null;
             $leve->CompanyLeve      = null;
             $leve->GatheringLeve    = null;
             $leve->BattleLeve       = null;
 
             // CraftLeve = 917500 > 930000
             if ($leve->DataIdTargetID >= 917500 && $leve->DataIdTargetID <= 930000) {
-                $leve->CraftLeve = Redis::Cache()->get("xiv_CraftLeve_{$leve->DataIdTargetID}");
+                $leve->CraftLeve = $leve->DataId;
             }
 
             // CompanyLeve = 196600 > 196700
             if ($leve->DataIdTargetID >= 196600 && $leve->DataIdTargetID <= 199000) {
-                $leve->CompanyLeve = Redis::Cache()->get("xiv_CompanyLeve_{$leve->DataIdTargetID}");
+                $leve->CompanyLeve = $leve->DataId;
             }
 
             // GatheringLeve = 131070 > 131300
             if ($leve->DataIdTargetID >= 131070 && $leve->DataIdTargetID <= 135000) {
-                $leve->GatheringLeve = Redis::Cache()->get("xiv_GatheringLeve_{$leve->DataIdTargetID}");
+                $leve->GatheringLeve = $leve->DataId;
             }
 
             // BattleLeve = 65530 > 65800
             if ($leve->DataIdTargetID >= 65530 && $leve->DataIdTargetID <= 70000) {
-                $leve->BattleLeve = Redis::Cache()->get("xiv_BattleLeve_{$leve->DataIdTargetID}");
+                $leve->BattleLeve = $leve->DataId;
             }
 
             unset($leve->DataId);
             $leve->{$leve->DataIdTarget . "TargetID"} = $leve->DataIdTargetID;
-            unset($leve->DataIdTargetID);
 
             // ---------------------------------------------------
             Redis::Cache()->set($key, $leve, self::REDIS_DURATION);
